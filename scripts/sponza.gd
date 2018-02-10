d9a1f39eb33cdad63b6d11e7a1f5822eea58a0ae
Bastiaan Olij <mux213@gmail.com>
2018-02-10 23:14:31 +1100

    Made Oculus version

--- scripts/sponza.gd
+++ scripts/sponza.gd
@@ -1,9 +1,8 @@
 extends WorldEnvironment
 
 func _ready():
-	var interface = ARVRServer.find_interface("OpenVR")
+	var interface = ARVRServer.find_interface("Oculus")
 	if interface and interface.initialize():
 		get_viewport().arvr = true
-		get_viewport().hdr = false
 		
 		$OVRFirstPerson/Right_Hand.connect("button_pressed", $OVRFirstPerson/HUD_Anchor/Settings_VR, "_on_Right_Hand_button_pressed")
